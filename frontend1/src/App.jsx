41e76b4ee3b141b75a59141d5998ada5e1922250
kartik <kartiksaxena.wps@gmail.com>
2025-04-10 10:50:13 -0600

    Updated routes for components

--- frontend1/src/App.jsx
+++ frontend1/src/App.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import {Routes, Route} from "react-router-dom"
 import BookSearch from "./components/booksearch";
 import BookDetails from "./components/booksdetail";
+import SignUp from "./components/SignUp";
+import LandingPage from "./components/LandingPage";
+import Login from "./components/LogIn";
 
 
 export default function App() {
   return (
     <div className="p-3 mb-2 bg-success text-white min-vh-100">
-      <h1 className="text-center my-4">Library Management System</h1>
+      
       <div className="d-flex justify-content-center">
         <Routes>
-          <Route path="/" element={<BookSearch />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/search" element={<BookSearch />} />
           <Route path="/book/:isbn" element={<BookDetails />} />
         </Routes>
       </div>
